2917b75f83e79fee10e1e9f1321b90adde9f5886
Lesedi Molemi <molemilesedi@gmail.com>
2024-02-28 13:59:08 +0200

    Minor adjustments (#11)
    * Update ProjectsSummary.tsx
    
    Switched Varsity Registration and Techspo Website project so that Varsit Registration comes before Techspo Website
    
    * Update Project.tsx
    
    Changed word order and spelling

--- src/Components/CloudResume/Project.tsx
+++ src/Components/CloudResume/Project.tsx
@@ -8,7 +8,7 @@ function Project() {
         title="Cloud Resume Challenge"
         link="https://github.com/Lesmol/CloudResumeChallenge_Azure"
         stack="React | Azure App Service | GitHub Actions"
-        description="This project is the website your on right now. I used React for the frontend and 
+        description="This project is the website you're on right now. I used React for the frontend and 
         it's hosted on an Azure App Service. For CI/CD, I made use of GitHub Actions to automatically
          test and deploy the code to the App Service once pushed to main. For more information on the project, 
          click on the title, you'll be redirected to the repo where you can read more in the README file."
@@ -16,21 +16,21 @@ function Project() {
       <ProjectInfo
         title="Varsity Registration"
         link="https://github.com/Lesmol/varsity_registration"
-        stack="Next.js | Firebase Auth | TS"
+        stack="Next.js | Firebase Auth | TypeScript"
         description="A Next.js web application that allows for an automated tertiary application experience to any tertiary 
-        institution we offer. This is the frontend repo built in Next.js and TypeScript. The application is still in 
-        development, but we have managed to add user authentication by making use of Firbases Auth service."
+        institution we offer. This is the frontend repo built with Next.js and TypeScript. The application is still in 
+        development, but we have managed to add user authentication by using Firebase Auth service."
       />
       <ProjectInfo
         title="Techspo Site"
         link="https://github.com/Lesmol/Techspo_Website"
         stack="Azure SQL Database | ASP.NET | C#"
         description="This responsive MVC application was developed using ASP.NET, the C# framework. Additionally, I designed 
-        some of the application's assets with Figma to ensure they match with the website's theme. 
+        some of the application's assets with Figma to ensure they match the website's theme. 
         I used Azure for the database server and web hosting. Using Azure for the database 
-        server proved beneficial, since it was a group effort, it allowed seamless access 
+        server proved beneficial since it was a group effort, it allowed seamless access 
         from different machines within the subnet range I had set up. For more information on the 
-        project, click on the title, you'll be redirected to the repo where you can read more in the README file."
+        project, click on the title, and you'll be redirected to the repo where you can read more in the README file."
       />
     </div>
   );
