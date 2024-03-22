36968a8f523a2d8b6856da7145a03680845c69da
Lesedi Molemi <molemilesedi@gmail.com>
2024-03-22 22:24:19 +0200

    Added rel="noopener noreferrer" to every _blank link in the project

--- src/Components/Home/ArticleItem.tsx
+++ src/Components/Home/ArticleItem.tsx
@@ -8,7 +8,12 @@ function ArticleItem(props: Props) {
   return (
     <div className="items-center mt-4 md:space-x-3 md:flex md:flex-row-reverse">
       <p className="text-darker-primary w-[180px]">{props.date}</p>
-      <a href={props.link} target="_blank" className="hover:text-primary">
+      <a
+        href={props.link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-primary"
+      >
         {props.title}
       </a>
     </div>
