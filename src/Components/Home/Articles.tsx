f6a34b6355349757416b58d7463085e7ca5b3945
Lesedi <molemilesedi@gmail.com>
2024-04-08 20:00:43 +0200

    - added new article - removed date from article item - added arrow to article item

--- src/Components/Home/Articles.tsx
+++ src/Components/Home/Articles.tsx
@@ -1,14 +1,16 @@
 import ArticleItem from "./ArticleItem";
 
-
 function Articles() {
   return (
-    <div className="mt-8">
-      <h3 className="text-primary text-xl">Articles</h3>
+    <div className="mt-12">
+      <h3 className="text-primary text-xl mb-4">Articles</h3>
       <ArticleItem
         title="Provisioning Azure resources with terraform within GitHub Actions"
         link="https://www.linkedin.com/pulse/my-experience-provisioning-azure-resources-using-terraform-molemi-li7ff/?trackingId=%2FMLQ%2BpTYSWCWLwLM4l0wXA%3D%3D"
-        date="21 Mar 2024"
+      />
+      <ArticleItem
+        title="What we did wrong in this project: PHP, Tailwind, Git & GitHub"
+        link="https://www.linkedin.com/pulse/what-we-did-wrong-project-php-tailwind-git-github-lesedi-molemi-u3pgf/?trackingId=mjMULJOQSauumFJuQBEADw%3D%3D"
       />
     </div>
   );
