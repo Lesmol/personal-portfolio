806a12d6a2300bf2104aabdb3cab4d002bbecf01
Lesedi <molemilesedi@gmail.com>
2024-04-08 20:31:12 +0200

    Updated the look of projects and articles section in home page

--- src/Components/Home/ArticleItem.tsx
+++ src/Components/Home/ArticleItem.tsx
@@ -5,14 +5,14 @@ type Props = {
 
 function ArticleItem(props: Props) {
   return (
-    <div className="mb-3">
+    <div className="mb-4 md:mb-3">
       <a
         href={props.link}
         target="_blank"
         rel="noopener noreferrer"
         className="md:hover:text-primary"
       >
-        <span className=" mr-2">&#x2197;</span>
+        <span className="mr-2">&#x2197;</span>
         {props.title}
       </a>
     </div>
