6946efe8cad9cef17a73eb1cc4962497fdab0d53
Lesedi <molemilesedi@gmail.com>
2024-08-12 22:51:13 +0200

    - Added a new project - Rename existing projects and changed project descriptions in Resume page - changed deployment workflow to "deploy-app.yml"

--- src/Components/Home/ProjectsSummary.tsx
+++ src/Components/Home/ProjectsSummary.tsx
@@ -8,11 +8,15 @@ function ProjectsSummary() {
         Public repo's of projects I'm either working on or completed.
       </p>
       <LinkItem
-        title="Cloud Resume Challenge"
+        title="Responsive React App deployed on Azure"
         link="https://github.com/Lesmol/CloudResumeChallenge_AWS"
       />
       <LinkItem
-        title="Techspo Website"
+        title="Next.js Frontend with .NET Core Minimal API Backend"
+        link="https://github.com/Lesmol/zupi-site"
+      />
+      <LinkItem
+        title="Full-stack .NET Core MVC application with Azure hosted MS SQL Server Database"
         link="https://github.com/Lesmol/Techspo_Website"
       />
     </div>
--- src/Components/Resume/Project.tsx
+++ src/Components/Resume/Project.tsx
@@ -5,24 +5,33 @@ function Project() {
     <div className="mt-4">
       <h1 className="font-bold text-2xl text-primary">Projects</h1>
       <ProjectInfo
-        title="Cloud Resume Challenge"
+        title="Responsive React App"
         link="https://github.com/Lesmol/CloudResumeChallenge_Azure"
         stack="React | Azure App Service | GitHub Actions"
-        description="This project is the website you're on right now. I used React for the frontend and 
-        it's hosted on an Azure App Service. For CI/CD, I made use of GitHub Actions to automatically
-         test and deploy the code to the App Service once pushed to main. For more information on the project, 
-         click on the title, you'll be redirected to the repo where you can read more in the README file."
+        description={[
+          "Directed the migration project that transitioned the application to Vite, resulting in a decreased bundle size, optimised performance and reducing server build time.",
+          "Designed and implemented a robust CI/CD pipeline that streamlined testing and automated builds upon code commits, reducing deployment time and enhancing code quality.",
+          "Deployed a high-availability application on Azure using Azure App Service, ensuring 99.9% uptime and improving the scalability and reliability of the project.",
+        ]}
       />
       <ProjectInfo
-        title="Techspo Site"
+        title="Next.js Frontend with .NET Core Minimal API Backend"
+        link="https://github.com/Lesmol/zupi-site"
+        stack="Azure SQL Database | ASP.NET Core | C#"
+        description={[
+          "Designed and deployed scalable Azure infrastructure using Terraform for IaC and implemented Azure Pipelines for CI/CD, enhancing infrastructure deployment efficiency and reliability.",
+          "Developed a streamlined backend API using Minimal API architecture, significantly reducing boilerplate code and accelerating development time while maintaining high performance.",
+          "Engineered a responsive frontend with Next.js, leveraging SSR and server functions to enhance security and performance while delivering a seamless user experience.",
+        ]}
+      />
+      <ProjectInfo
+        title="Full-stack .NET Core MVC application with Azure hosted MS SQL Server Database"
         link="https://github.com/Lesmol/Techspo_Website"
         stack="Azure SQL Database | ASP.NET Core | C#"
-        description="This responsive MVC application was developed using ASP.NET, the C# framework. Additionally, I designed 
-        some of the application's assets with Figma to ensure they match the website's theme. 
-        I used Azure for the database server and web hosting. Using Azure for the database 
-        server proved beneficial since it was a group effort, it allowed seamless access 
-        from different machines within the subnet range I had set up. For more information on the 
-        project, click on the title, and you'll be redirected to the repo where you can read more in the README file."
+        description={[
+          "Developed a comprehensive full-stack .NET Core MVC application using Blazor Pages, enhancing user experience and delivering a responsive, feature-rich interface.",
+          "Implemented a secure, cloud-hosted database solution using Azure SQL Database, enabling seamless access for group members, which improved collaboration and data integrity.",
+        ]}
       />
     </div>
   );
