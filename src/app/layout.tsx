3d2bbde743e3c982833c8fa9c064f60bda953880
Lesedi <molemilesedi@gmail.com>
2026-01-27 19:27:12 +0200

    chore: optimize images with next/image, add SEO metadata, and fix navbar typos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,28 @@ export const metadata: Metadata = {
   icons: {
     icon: "https://cloudresumeresources.blob.core.windows.net/resumeimages/logo.svg",
   },
+  openGraph: {
+    title: "Lesedi Molemi",
+    description:
+      "Hi there👋🏾, I'm Lesedi. Welcome to my website where I manipulate the information to show you what I want you to see😈",
+    siteName: "Lesedi Molemi",
+    images: [
+      {
+        url: "https://cloudresumeresources.blob.core.windows.net/resumeimages/logo.svg",
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Lesedi Molemi",
+    description:
+      "Hi there👋🏾, I'm Lesedi. Welcome to my website where I manipulate the information to show you what I want you to see😈",
+    images: ["https://cloudresumeresources.blob.core.windows.net/resumeimages/logo.svg"],
+  },
 };
 
 export default function RootLayout({
